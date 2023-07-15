5a143c04e2de8c133a8ae218b0f3f5f97615995a
Connor Abbas <abbasconnor@gmail.com>
2023-07-15 13:27:29 -0500

    tests

--- tests/Feature/Controllers/EventControllerTest.php
+++ tests/Feature/Controllers/EventControllerTest.php
@@ -7,11 +7,11 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Event;
 use App\Models\EventType;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class EventControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_search_returns_302_when_logged_out(): void
     {
@@ -23,6 +23,34 @@ class EventControllerTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
         $this->get(route('events.search'))->assertStatus(200);
+    }
+
+    public function test_get_returns_302_when_logged_out()
+    {
+        $this->get(route('events.single', ['id' => 1]))->assertStatus(302);
+    }
+
+    public function test_get_returns_403_when_different_user()
+    {
+        $user = User::factory()->create();
+        $differentUser = User::factory()->create();
+        $event = Event::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->actingAs($differentUser);
+        $this->get(route('events.single', ['id' => $event->id]))->assertStatus(403);
+    }
+
+    public function test_get_returns_200_with_correct_user()
+    {
+        $user = User::factory()->create();
+        $event = Event::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->actingAs($user);
+        $this->get(route('events.single', ['id' => $event->id]))->assertStatus(200);
     }
 
     public function test_store_returns_302_when_logged_out(): void
@@ -70,22 +98,48 @@ class EventControllerTest extends TestCase
         $this->patch(route('events.update', ['id' => 123]))->assertStatus(302);
     }
 
-    // TODO: assert different data
-    public function test_update_returns_200_when_logged_in(): void
+    public function test_update_returns_403_with_different_user(): void
     {
         $user = User::factory()->create();
+        $differentUser = User::factory()->create();
         $event = Event::factory()->create([
             'user_id' => $user->id
         ]);
         $start = Carbon::now();
         $finish = Carbon::now()->addHour();
 
-        $this->actingAs($user);
+        $this->actingAs($differentUser);
         $this->patch(route('events.update', ['id' => $event->id]), [
             'start_time' => $start,
             'finish_time' => $finish,
             'event_type_id' => $event->type->id,
             'comments' => 'testing'
+        ])->assertStatus(403);
+    }
+
+    public function test_update_returns_200_with_correct_user(): void
+    {
+        $user = User::factory()->create();
+        $eventType = EventType::factory()->create();
+        $event = Event::factory()->create([
+            'user_id' => $user->id
+        ]);
+        $start = Carbon::now();
+        $finish = Carbon::now()->addHour();
+        $eventBefore = $event;
+
+        $this->actingAs($user);
+        $this->patch(route('events.update', ['id' => $event->id]), [
+            'start_time' => $start,
+            'finish_time' => $finish,
+            'event_type_id' => $eventType->id,
+            'comments' => 'testing 123'
         ])->assertStatus(200);
+
+        $eventAfter = $event->fresh();
+        $this->assertTrue($eventBefore->start_time != $eventAfter->start_time);
+        $this->assertTrue($eventBefore->finish_time != $eventAfter->finish_time);
+        $this->assertTrue($eventBefore->event_type_id != $eventAfter->event_type_id);
+        $this->assertTrue($eventBefore->comments != $eventAfter->comments);
     }
 }
