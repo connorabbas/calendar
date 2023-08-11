d83625605a42eb386a38b20c5b91079e61861ce1
Connor Abbas <abbasconnor@gmail.com>
2023-08-11 16:49:48 -0500

    run pint

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,8 @@
 <?php
 
 if (! function_exists('is_active_route')) {
-    function is_active_route(string $route): string {
-        return (request()->routeIs($route)) ? ' active ' : ''; 
+    function is_active_route(string $route): string
+    {
+        return (request()->routeIs($route)) ? ' active ' : '';
     }
 }
